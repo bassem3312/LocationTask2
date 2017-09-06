f64f486ad20190c18cd18b545c0d4330b28c70c6
Bassem3312 <basem3312@gmail.com>
2017-09-06 21:12:55 +0200

    Commit after adding Constants Class

---
+++ app/src/main/java/com/bey2ollak/locationtask/locationtask/utilities/Constants.java
@@ -0,0 +1,13 @@
+package com.bey2ollak.locationtask.locationtask.utilities;
+
+/**
+ * @author Bassem Mohsen : basem3312@gmail.com on 9/6/2017.
+ */
+
+public class Constants {
+    public  static  String API_BASE_DOMAIN="http://bey2ollak-places-task.eu-west-1.elasticbeanstalk.com/api/";
+    public  static  String PLASES_API="json/places";
+    public  static  String PAGE_PARAM="page";
+    public  static  String SIZE_PARAM="Size";
+
+}
