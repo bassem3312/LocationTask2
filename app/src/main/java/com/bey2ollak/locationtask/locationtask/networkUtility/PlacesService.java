f2fca09a41162b8af3681dc70c81922f188acd8c
Bassem3312 <basem3312@gmail.com>
2017-09-06 21:12:10 +0200

    Commit after implementing  get placeService interface

---
+++ app/src/main/java/com/bey2ollak/locationtask/locationtask/networkUtility/PlacesService.java
@@ -0,0 +1,28 @@
+package com.bey2ollak.locationtask.locationtask.networkUtility;
+
+import com.bey2ollak.locationtask.locationtask.models.Bey2ollakPlacesResult;
+import com.bey2ollak.locationtask.locationtask.utilities.Constants;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * @author Bassem Mohsen : basem3312@gmail.com on 9/6/2017.
+ */
+
+public interface PlacesService {
+
+    @GET("json/places")
+    Call<Bey2ollakPlacesResult> getPlaces(
+            @Query("page") int page,
+            @Query("size") int size);
+
+
+    public static final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(Constants.API_BASE_DOMAIN)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
